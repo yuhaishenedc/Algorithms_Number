3fdfd4d4e23ca398466b6f92fa13f41a904ba6cf
yuhaishen <2249094924@qq.com>
2020-08-02 14:33:57 +0800

    添加了快速幂

---
+++ code/power/Binary_Exponentiation.java
@@ -0,0 +1,42 @@
+package power;
+
+public class Binary_Exponentiation {
+
+    public static void main(String[] args) {
+        System.out.println(pow_mod(2,10,2047));
+    }
+
+
+    //快速幂算法
+    public static int pow(int a,int b){
+        int ret=1;
+        int base=a;
+        while(b>0){
+            if((b&1)!=0){
+                ret*=base;
+            }
+            base*=base;
+            b>>=1;
+        }
+        return ret;
+    }
+
+    //快速幂求余
+    public static int pow_mod(int a,int b,int c){
+        int ret=1;
+        int base=a%c;
+        while(b>0){
+            if((b&1)!=0){
+                ret=ret*base%c;
+            }
+            base=base*base%c;
+            b>>=1;
+        }
+        return ret;
+    }
+
+    //需要用到的知识是如下两个：
+    //(a + b) % m = (a % m+b % m) % m;
+    //a * b % m = ((a % m)*(b % m)) % m;
+
+}
